e5e7348507823611230b7ee93c25f0758c821076
Vinicius Salvati Melquiades <vinicius.melquiades@outlook.com>
2015-05-22 02:30:48 -0300

    Gzip
    Now, the minified file will also be Gziped and put in the output folder too.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var ts = require('gulp-typescript');
 var merge = require('merge2');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var gzip = require('gulp-gzip');
 
 var tsProject = ts.createProject({
 	out: 'ngMaterialize.js',
@@ -20,12 +21,14 @@ gulp.task('compile', function () {
 		.pipe(gulp.dest('dist'))
 		.pipe(uglify())
 		.pipe(concat('ngMaterialize.min.js')) // lazy way to rename the output :)
+		.pipe(gulp.dest('dist'))
+		.pipe(gzip({ append: true }))
 		.pipe(gulp.dest('dist'));
-		
+
 	var dts = merge(gulp.src('src/dts_before'), tsPipes.dts, gulp.src('src/dts_after'))
 		.pipe(concat('ngMaterialize.d.ts'))
 		.pipe(gulp.dest('dist'));
-	
+
 	return merge([js, dts]);
 });
 
