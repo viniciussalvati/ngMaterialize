a03870cf70dd934f25a04f622abf0e5cba281999
Vinicius Salvati Melquiades <vinicius.melquiades@outlook.com>
2015-05-22 02:24:02 -0300

    Minify
    Now the output is minified to the ngMaterialize.min.js file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var merge = require('merge2');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 var tsProject = ts.createProject({
 	out: 'ngMaterialize.js',
@@ -15,13 +16,17 @@ gulp.task('compile', function () {
 		.pipe(ts(tsProject));
 
 	var js = merge(gulp.src('src/js_before'), tsPipes.js, gulp.src('src/js_after'))
-		.pipe(concat('ngMaterialize.js'));
+		.pipe(concat('ngMaterialize.js'))
+		.pipe(gulp.dest('dist'))
+		.pipe(uglify())
+		.pipe(concat('ngMaterialize.min.js')) // lazy way to rename the output :)
+		.pipe(gulp.dest('dist'));
 		
 	var dts = merge(gulp.src('src/dts_before'), tsPipes.dts, gulp.src('src/dts_after'))
-		.pipe(concat('ngMaterialize.d.ts'));
-	
-	return merge([js, dts])
+		.pipe(concat('ngMaterialize.d.ts'))
 		.pipe(gulp.dest('dist'));
+	
+	return merge([js, dts]);
 });
 
 gulp.task('build', ['compile']);
