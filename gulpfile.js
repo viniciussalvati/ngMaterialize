23ca9a6552f0c08e470a28e9c4cc829b065720b9
Vinicius Salvati Melquiades <vinicius.melquiades@outlook.com>
2015-05-27 00:52:55 -0300

    Messed up with the gulpfile.
     - It "should" have a performance gain with these changes, since I'm only writing to the disk once.
     - Either way, the code is easier to read.
     - I also made all the resulting files be compiled to the "build" folder. They only go to the "dist" folder if the "--release" or "-r" flag is passed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
+/* global process */
 var gulp = require('gulp');
+var util = require('gulp-util');
 var ts = require('gulp-typescript');
-var merge = require('merge2');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var gzip = require('gulp-gzip');
 var replace = require('gulp-replace');
+var rename = require('gulp-rename');
+var clone = require('gulp-clone');
+var es = require('event-stream');
 
 var tsProject = ts.createProject({
 	out: 'ngMaterialize.js',
@@ -13,29 +17,52 @@ var tsProject = ts.createProject({
 	target: 'ES5'
 });
 
+var release = !!util.env.release || util.env.r;
+
+function handleJsResults(jsPipe) {
+	var js = es.merge(gulp.src('src/js_before'), jsPipe, gulp.src('src/js_after'))
+		.pipe(concat('ngMaterialize.js'));
+	
+	var min = js
+		.pipe(clone())
+		.pipe(rename({ extname: '.min.js' }))
+		.pipe(uglify());
+
+	var gz = min
+		.pipe(clone())
+		.pipe(gzip({ append: true }));
+
+	return es.merge(js, min, gz);
+}
+
+function handleDtsResults(dtsPipe) {
+	var replacedText = dtsPipe.pipe(replace('declare ', ''));
+
+	return es.merge(gulp.src('src/dts_before'), replacedText, gulp.src('src/dts_after'))
+		.pipe(concat('ngMaterialize.d.ts'));
+}
+
 gulp.task('compile', function () {
 	var tsPipes = gulp.src(['src/ngMaterialize.ts', 'src/**/*.ts'])
 		.pipe(ts(tsProject));
 
-	var js = merge(gulp.src('src/js_before'), tsPipes.js, gulp.src('src/js_after'))
-		.pipe(concat('ngMaterialize.js'))
-		.pipe(gulp.dest('dist'))
-		.pipe(uglify())
-		.pipe(concat('ngMaterialize.min.js')) // lazy way to rename the output :)
-		.pipe(gulp.dest('dist'))
-		.pipe(gzip({ append: true }))
-		.pipe(gulp.dest('dist'));
+	var js = handleJsResults(tsPipes.js);
 
-	var dts = tsPipes.dts
-		.pipe(replace('declare ', ''));
-	dts = merge(gulp.src('src/dts_before'), tsPipes.dts, gulp.src('src/dts_after'))
-		.pipe(concat('ngMaterialize.d.ts'))
-		.pipe(gulp.dest('dist'));
+	var dts = handleDtsResults(tsPipes.dts);
 
-	return merge([js, dts]);
+	return es.merge([js, dts])
+		.pipe(gulp.dest('build'));
 });
 
-gulp.task('build', ['compile']);
+gulp.task('build', ['compile'], function () {
+	if (release) {
+		return gulp
+			.src('build/*')
+			.pipe(gulp.dest('dist'));
+	} else {
+		util.log(util.colors.yellow.bold("Not in release mode. Won't copy files to production directory"));
+	}
+});
 
 gulp.task('watch', ['build'], function () {
 	gulp.watch('src/*.ts', ['build']);
