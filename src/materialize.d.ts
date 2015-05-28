5077c837c2c3d42818d9349d4588c695eb65db72
Vinicius Salvati Melquiades <vinicius.melquiades@outlook.com>
2015-05-28 14:25:24 -0300

    Created the `dropdown` directive. It's only a wrapper around the default dropdown.

--- src/materialize.d.ts
+++ src/materialize.d.ts
@@ -1,6 +1,19 @@
 interface JQuery {
 	openModal(options?: any): JQuery
 	closeModal(options?: any): JQuery
-		
+
 	material_select(destroy?: string): JQuery
+
+	dropdown(options: materialize.IDropdownOptions): JQuery
+}
+
+declare module materialize {
+	interface IDropdownOptions {
+		inDuration?: number;
+		outDuration?: number;
+		constrain_width?: boolean;
+		hover?: boolean;
+		gutter?: number;
+		bellowOrigin?: boolean;
+	}
 }
