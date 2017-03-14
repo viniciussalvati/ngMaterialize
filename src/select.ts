32b4e6e2cb855050009b489980be8aaea2ceb21a
Vinicius Salvati Melquiades <vinicius.melquiades@outlook.com>
2017-03-14 11:39:59 -0300

    Fixed a problem for the select in newer versions of angular or if its recreated

--- src/select.ts
+++ src/select.ts
@@ -1,29 +1,36 @@
 MaterialSelect.$inject = ['$timeout'];
 
-function MaterialSelect($timeout: ng.ITimeoutService): ng.IDirective{
-		var directive = {
-			link: link,
-			restrict: 'E',
-			require: '?ngModel'
+function MaterialSelect($timeout: ng.ITimeoutService): ng.IDirective {
+	var directive = {
+		link: link,
+		restrict: 'E',
+		require: '?ngModel'
+	};
+
+	function link(scope, element: JQuery, attrs, ngModel) {
+		let created = false;
+
+		const create = () => {
+			created && destroy();
+			element.material_select();
+			created = true;
 		};
 
-		function link(scope, element: JQuery, attrs, ngModel) {
-			$timeout(create);
+		const destroy = () => {
+			element.material_select('destroy');
+			element.siblings('span.caret').remove();
+		};
 
-			if (ngModel) {
-				ngModel.$render = create;
-			}
+		$timeout(create);
 
-			function create() {
-				element.material_select();
-			}
-
-			element.one('$destroy', function () {
-				element.material_select('destroy');
-			});
+		if (ngModel) {
+			ngModel.$render = create;
 		}
 
-		return directive;
+		element.one('$destroy', destroy);
+	}
+
+	return directive;
 }
 
 ngMaterialize.directive('select', MaterialSelect);
